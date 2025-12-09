007b4f3d8c71371b40a44464a5d257178b115a3a
Juan Puerta <juan.pablo.puerta@correounivalle.edu.co>
2025-12-09 03:08:59 -0500

    Adding some terrain details and character controller fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,37 +2,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    [SerializeField] private float speed = 5f;        
-    public float groundDist = 0.3f; 
-    public LayerMask terrainLayer;  
+    [SerializeField] private float speed = 3f;     
+    [SerializeField] private float gravity = 20f;    
     
-    public Rigidbody rb;
+    private CharacterController cc; 
     public SpriteRenderer sr;
     [SerializeField] private Animator anim;
-    
-    
-    private Vector3 moveInput;
+
+    private Vector3 moveDirection; 
 
     void Start()
     {
-        
-        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CharacterController>();
         sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
 
-        if (rb == null) Debug.LogError("Rigidbody component is missing on the Player!");
-        if (sr == null) Debug.LogError("SpriteRenderer component is missing on the Player!");
+        if (cc == null) Debug.LogError("CharacterController component is missing!");
+        if (sr == null) Debug.LogError("SpriteRenderer component is missing!");
     }
 
     void Update()
     {
-        
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        moveInput = new Vector3(x, 0, y);
 
-        
+        if (cc.isGrounded)
+        {
+            moveDirection = new Vector3(x, 0, y);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            
+            moveDirection.y = -gravity * Time.deltaTime; 
+
+        }
+        else
+        {
+
+            moveDirection.x = x * speed;
+            moveDirection.z = y * speed;
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        cc.Move(moveDirection * Time.deltaTime);
+
         if (x < 0)
         {
             sr.flipX = true;
@@ -42,42 +54,11 @@ public class PlayerController : MonoBehaviour
             sr.flipX = false;
         }
 
-       
         if (anim != null)
         {
+  
             bool isMoving = Mathf.Abs(x) > 0.01f || Mathf.Abs(y) > 0.01f;
             anim.SetBool("isRunning", isMoving);
-        }
-        else
-        {
-            anim.SetBool("isRunning", false);
-        }
-    }
-
-    void FixedUpdate()
-    {
-      
-        RaycastHit hit;
-        Vector3 castPos = transform.position;
-        castPos.y += 1; 
-        
-        float currentYVelocity = rb.linearVelocity.y;
-        
-        bool isGrounded = Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer);
-
-        if (isGrounded)
-        {
-
-            Vector3 movePos = transform.position; 
-            movePos.y = hit.point.y + groundDist;
-            transform.position = movePos;
-
-            rb.linearVelocity = new Vector3(moveInput.x * speed, 0, moveInput.z * speed);
-        }
-        else
-        {
-
-            rb.linearVelocity = new Vector3(moveInput.x * speed, currentYVelocity, moveInput.z * speed);
         }
     }
 }
